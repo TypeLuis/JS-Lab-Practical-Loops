d40f5488ccd75297f51420ee06ebbbc229a7c6b8
TypeLuis <typeluis@gmail.com>
2025-12-04 21:54:15 +0000

    function

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-const fizzBuzz = {
-    fizz : [],
-    buzz : [],
-    fizzBuzz : [],
-    noFizzBuzz : []
-}
-for(let i = 1; i <= 100; i++){
-    if(i % 3 === 0 && i % 5 === 0)fizzBuzz.fizzBuzz.push(i)
-    else if(i % 3 === 0) fizzBuzz.fizz.push(i)
-    else if(i % 5 === 0) fizzBuzz.buzz.push(i)
-    else fizzBuzz.noFizzBuzz.push(i)
+const fizzBuzz = () => {
+    const fizzBuzz = {
+        fizz : [],
+        buzz : [],
+        fizzBuzz : [],
+        noFizzBuzz : []
+    }
+    for(let i = 1; i <= 100; i++){
+        if(i % 3 === 0 && i % 5 === 0)fizzBuzz.fizzBuzz.push(i)
+        else if(i % 3 === 0) fizzBuzz.fizz.push(i)
+        else if(i % 5 === 0) fizzBuzz.buzz.push(i)
+        else fizzBuzz.noFizzBuzz.push(i)
+    }
+    return fizzBuzz
 }
 
-console.log(fizzBuzz)
+console.log(fizzBuzz())
